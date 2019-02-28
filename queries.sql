cddd52a1008891e12013bfddbab15ee7d52abde7
Igor B <blinovigo@yandex.ru>
2019-02-28 09:02:09 +0300

    Исправил задание 10

--- queries.sql
+++ queries.sql
@@ -1,22 +1,86 @@
-INSERT INTO project SET name = 'Входяшие', user_id = '1';
-INSERT INTO project SET name = 'Учеба', user_id = '1';
-INSERT INTO project SET name = 'Работа', user_id = '1';
-INSERT INTO project SET name = 'Домашние дела', user_id = '1';
-INSERT INTO project SET name = 'Авто', user_id = '1';
-INSERT INTO project SET name = 'Входяшие', user_id = '2';
+INSERT INTO project (
+	name,
+	user_id
+	)
+VALUES (
+	'Входящие',
+	'1'
+),
+(
+	'Учеба',
+	'1'
+),
+(
+	'Работа',
+	'1'
+),
+(
+	'Домашние дела',
+	'1'
+),
+(
+	'Авто',
+	'1'
+),
+(
+	'Входящие',
+	'2'
+);
 
-INSERT INTO user SET email = 'list@mail.ru', name = 'One_user';
-INSERT INTO user SET email = 'list@mail.ru', name = 'Two_user';
+INSERT INTO user (
+	email,
+	name
+)
+VALUES (
+	'list@mail.ru',
+	'One_user'
+),
+(
+	'list@mail.ru',
+	'Two_user'
+);
 
-INSERT INTO task SET name = 'Собеседование в IT компании', term = '01.12.2019', project_id = '3'; 
-INSERT INTO task SET name = 'Выполнить тестовое задание', term = '25.12.2019', project_id = '3';
-INSERT INTO task SET name = 'Сделать задание первого раздела', term = '21.12.2019', project_id = '2';
-INSERT INTO task SET name = 'Встреча с другом', term = '22.12.2019', project_id = '1';
-INSERT INTO task SET name = 'Купить корм для кота', project_id = '4';
-INSERT INTO task SET name = 'Заказать пиццу', project_id = '4';
+INSERT INTO task (
+	name,
+	term,
+	project_id
+)
+VALUES (
+	'Собеседование в IT компании',
+	'01.12.2019',
+	'3'
+),
+(
+	'Выполнить тестовое задание',
+	'25.12.2019',
+	'3'
+),
+(
+	'Сделать задание первого раздела',
+	'21.12.2019',
+	'2'
+),
+(
+	'Встреча с другом',
+	'22.12.2019',
+	'1'
+);
+
+INSERT INTO task (
+	name,
+	project_id
+)
+VALUES(
+	'Купить корм для кота',
+	'4'
+),
+(
+	'Заказать пиццу',
+	'4'
+);
 
 
 SELECT id, name FROM project WHERE user_id = '2';
 SELECT id, name FROM task WHERE project_id = '3';
-INSERT INTO task SET data_finish = '22.12.2019' WHERE name = 'Встреча с другом';
+UPDATE task SET data_finish = '22.12.2019' WHERE id = '2';
 UPDATE task SET name = 'Новое название' WHERE id = '1';
